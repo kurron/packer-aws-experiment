f1204fc9a39c465298d2536f3721d6d9d8dce4d6
Ron Kurr <kurr@kurron.org>
2015-06-15 14:14:13 +0000

    Found a way to get Ansible installed

--- ansible/scripts/install-ansible.sh
+++ ansible/scripts/install-ansible.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-curl -O https://bootstrap.pypa.io/get-pip.py
-sudo --set-home python get-pip.py
-sudo --set-home pip install ansible 
+#sudo --set-home easy_install pip
+
+#sudo apt-get install -y build-essential dkms python-setuptools python-dev
+#curl -O https://bootstrap.pypa.io/get-pip.py
+#sudo --set-home python get-pip.py
+#sudo --set-home pip install ansible 
+
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get install -y ansible
