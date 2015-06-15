f97fd1824267cf9552877fd9a1d8c902e273d65e
Ron Kurr <kurr@kurron.org>
2015-06-15 15:06:12 +0000

    Docker is installed

--- ansible/scripts/install-ansible.sh
+++ ansible/scripts/install-ansible.sh
@@ -2,11 +2,13 @@
 
 #sudo --set-home easy_install pip
 
+# this form can't seem to get access to all the packages and fails with 404 sometimes
 #sudo apt-get install -y build-essential dkms python-setuptools python-dev
 #curl -O https://bootstrap.pypa.io/get-pip.py
 #sudo --set-home python get-pip.py
 #sudo --set-home pip install ansible 
 
+# this form was the only one that works in AWS
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get -y update
